3a9e8169ace5bdd8c3e47c2e1c9582b294dc2211
AlphaT7 <{ID}+{username}@users.noreply.github.com>
2024-01-23 17:00:29 -0600

    able to initialize a game connection and disconnect

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@ import { io } from "socket.io-client";
 const $ = document.querySelector.bind(document);
 const log = console.log.bind(console);
 
-// let domain =
-//   window.location.protocol + "//" + window.location.hostname + ":3000";
+let domain =
+  window.location.protocol + "//" + window.location.hostname + ":3000";
 
-const socket = io("http://localhost:3000", {
+const socket = io(domain, {
   withCredentials: false,
 });
 
 document.addEventListener("DOMContentLoaded", () => {
   socket.emit("test", "testing123");
+  socket.emit("latency", Date.now());
 });
 
 $("#canvas").style.display = "block";
@@ -27,4 +28,82 @@ $("#handle").addEventListener("click", () => {
     $("#handle").innerHTML = "chevron_left";
     panel.classList.toggle("showPanel");
   }
+});
+
+$("#creategame").addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formData = new FormData(e.target);
+  socket.emit("newgame", formData.get("gamename"));
+});
+
+$("#joingame").addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formData = new FormData(e.target);
+  socket.emit("joingame", formData.get("gamelist"));
+});
+
+/* Functions */
+
+function gamelistupdate(data) {
+  $("#gamelist").innerHTML = "";
+  data.forEach((name) => {
+    let option = new Option(name, name);
+    $("#gamelist").append(option);
+  });
+}
+
+function joingame(data) {
+  $("#gamename").value = "";
+  $("#gameid").value = data;
+  $("#creategamegroup").disabled = true;
+  $("#joingamegroup").disabled = true;
+}
+
+function newgame(data) {
+  $("#gamename").value = "";
+  $("#gameid").value = data;
+  $("#creategamegroup").disabled = true;
+  $("#joingamegroup").disabled = true;
+}
+
+function gameover() {
+  $("#gameid").value = "";
+  $("#creategamegroup").disabled = true;
+  $("#joingamegroup").disabled = true;
+}
+
+/* Socket.IO Events */
+
+socket.on("error", (data) => {
+  log(data);
+});
+
+socket.on("success", (data) => {
+  log(data);
+});
+
+socket.on("gamelistupdate", (data) => {
+  gamelistupdate(data);
+});
+
+socket.on("test", (data) => {
+  log(data);
+});
+
+socket.on("gameover", (data) => {
+  gameover();
+  log(data);
+});
+
+socket.on("latency", (time) => {
+  $("#latency").value = Date.now() - time;
+  //socket.emit("latency", Date.now());
+});
+
+socket.on("joingame", (data) => {
+  joingame(data);
+});
+
+socket.on("newgame", (data) => {
+  newgame(data);
 });
--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 const httpServer = require("http").createServer();
+const db = require("better-sqlite3")("sql.db");
+db.pragma("journal_mode = WAL");
+
 const io = require("socket.io")(httpServer, {
   cors: {
     origin: "http://localhost:3001",
@@ -6,16 +9,137 @@ const io = require("socket.io")(httpServer, {
   },
 });
 
+const log = console.log.bind(console);
+
 httpServer.listen(3000);
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+function newconnection(socket) {
+  log("user connected");
+  socket.emit("gamelistupdate", gamelist());
+}
 
-  socket.on("test", (details) => {
-    console.log(details);
+function gamelist() {
+  return db
+    .prepare("SELECT name FROM games WHERE live = false")
+    .pluck(true)
+    .all();
+}
+
+function newgame(gamename, socket) {
+  // on "newgame" event, first check for any special chars;
+  // end execution if found
+  let regex = new RegExp(/[^a-zA-Z0-9]/);
+  if (regex.test(gamename)) {
+    socket.emit("error", "Only alphanumeric characters are allowed!");
+    return;
+  }
+
+  // otherwise, check if the new game name exists in the database
+  let query = db.prepare("SELECT name FROM games").pluck(true).all();
+  let test = query.some((rs) => rs == gamename);
+
+  if (!test) {
+    // if the new gamename doesn't exist in the database, insert it
+    let insert = db.prepare(
+      "INSERT INTO games (name, bluestatus, redstatus) VALUES (?, ?, ?)"
+    );
+
+    insert.run(gamename, socket.id, "pending");
+    socket.join(gamename);
+
+    // broadcast gamelistupdate to all connections
+    io.emit("gamelistupdate", gamelist());
+    socket.emit("newgame", gamename);
+    socket.emit("success", "Blue player initalized. Waiting for red player...");
+  } else {
+    // otherwise notify the player that it is already in use;
+    socket.emit("error", "Game name already in use!");
+  }
+}
+
+function joingame(gamename, socket) {
+  // on "joingame" event, first check for any special chars;
+  // end execution if found
+  let regex = new RegExp(/[^a-zA-Z0-9]/);
+  if (regex.test(gamename)) {
+    socket.emit("error", "Only alphanumeric characters are allowed!");
+    return;
+  }
+
+  // otherwise, check if the new game name exists in the database
+  let query = db.prepare("SELECT name FROM games").pluck(true).all();
+  let test = query.some((rs) => rs == gamename);
+
+  if (test) {
+    // if the join gamename doesn't exist in the database, update it
+    let update = db.prepare(
+      "UPDATE games SET redstatus = (?), live = true WHERE name = (?)"
+    );
+    update.run(socket.id, gamename);
+    socket.join(gamename);
+    log(gamelist(), "gamelist");
+    io.emit("gamelistupdate", gamelist());
+    socket.emit("joingame", gamename);
+    socket.emit("success", "Red player initalized. Game is now Live!");
+  } else {
+    // otherwise notify the player that it is already in use;
+    socket.emit("error", "Game name does not exist!");
+  }
+}
+
+function endgame(socket) {
+  let query =
+    "SELECT * FROM games WHERE bluestatus = '" +
+    socket.id +
+    "' OR redstatus = '" +
+    socket.id +
+    "'";
+
+  let data = db.prepare(query).all();
+
+  if (data.length > 0) {
+    let room = data[0].name;
+    // delete the room record from the database
+    let DELETE = "DELETE FROM games WHERE name = '" + data[0].name + "'";
+    let deleterecord = db.prepare(DELETE);
+    deleterecord.run();
+
+    io.emit("gamelistupdate", gamelist());
+
+    // notify users the game has ended, and leave the socket.io room
+    io.to(room).emit("gameover", "Game over, your opponent has left.");
+    io.in(room).socketsLeave([room]);
+  }
+}
+
+function leaveroom(socket, room) {
+  socket.leave(room);
+}
+
+function latency(time, socket) {
+  socket.emit("latency", time);
+}
+
+io.on("connection", (socket) => {
+  newconnection(socket);
+
+  socket.on("newgame", (gamename) => {
+    newgame(gamename, socket);
+  });
+
+  socket.on("joingame", (gamename) => {
+    joingame(gamename, socket);
   });
 
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    endgame(socket);
+  });
+
+  socket.on("leaveroom", (room) => {
+    leaveroom(socket, room);
+  });
+
+  socket.on("latency", (time) => {
+    latency(time, socket);
   });
 });
